c02527c29025835fa3d9fe5c662bc78c747b2160
Utkarsh Pandey <theutkarshmail@gmail.com>
2024-04-15 21:10:40 +0530

    feat:screen share

--- app/videoCall/[chatId]/_components/MeetingRoom.tsx
+++ app/videoCall/[chatId]/_components/MeetingRoom.tsx
@@ -1,42 +1,81 @@
 import router from '@/backend/routes';
-import { CallControls, CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
+import { CallControls, CallParticipantsList, PaginatedGridLayout, ParticipantView, SfuModels, SpeakerLayout, StreamVideoParticipant, useCall, useCallStateHooks } from '@stream-io/video-react-sdk';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
 
 function MeetingRoom() {
     const router = useRouter();
-    const [layout,setLayout] = useState<'grid'|'speaker-left'|'speaker-right'>('grid');
-    const [showParticipants,setShowParticipants] = useState<boolean>(false); 
-    const CallLayout =()=>{
-        switch(layout){
+    const [layout, setLayout] = useState<'grid' | 'speaker-left' | 'speaker-right'>('grid');
+
+    const { useParticipants } = useCallStateHooks();
+    const [participantInSpotlight, ...otherParticipants] = useParticipants();
+    const CallLayout = () => {
+        switch (layout) {
             case 'grid':
-                return <PaginatedGridLayout/>
+                return <PaginatedGridLayout />
             case 'speaker-left':
-                return <SpeakerLayout participantsBarPosition={"right"}/>
+                return <SpeakerLayout participantsBarPosition={"right"} />
             default:
-                return <SpeakerLayout participantsBarPosition={"left"}/>
+                return <SpeakerLayout participantsBarPosition={"left"} />
         }
     }
-  return (
-    <section className='relative h-screen w-full overflow-hidden pt-4 '>
-      <div className='relative flex size-full items-center justify-center'>
-        <div className='flex size-full max-w-[1000px] items-center '>
-            <CallLayout/>
-        </div>
-        <div className={`h-calc(100vh-86px) ${showParticipants?"block":"hidden"} ml-2`}>
-            <CallParticipantsList onClose={()=>{
-                setShowParticipants(false);
-            }} />
-        </div>
-        <div className='fixed bottom-0 flex w-full items-center justify gap-5'>
-            <CallControls onLeave={()=>{
-                router.push('/')
-            }}/>
-        </div>
-        <button onClick={()=>{setShowParticipants(prev=>!prev)}} className='text-black'>click</button>
-      </div>
-    </section>
-  )
+    return (
+        <section className='relative h-screen w-full overflow-hidden pt-4 '>
+            <div className='relative flex size-full items-center justify-center'>
+                <div className='flex size-full max-w-[1000px] items-center '>
+                    {/* <CallLayout /> */}
+                </div>
+                <SpeakerLayout/>
+
+                <div className='fixed bottom-0 flex w-full items-center justify gap-5'>
+                    <CallControls onLeave={() => {
+                        router.push('/')
+                    }}
+                    />
+                </div>
+
+            </div>
+        </section>
+    )
 }
 
 export default MeetingRoom
+
+
+export const SpeakerView = () => {
+  const call = useCall();
+  const { useParticipants } = useCallStateHooks();
+  const [participantInSpotlight, ...otherParticipants] = useParticipants();
+
+  return (
+    // enables the default styling for the video SDK
+    <div className="speaker-view">
+      {call && otherParticipants.length > 0 && (
+        <div className="participants-bar">
+          {otherParticipants.map((participant) => (
+            <div className="participant-tile" key={participant.sessionId}>
+              <ParticipantView participant={participant} />
+            </div>
+          ))}
+        </div>
+      )}
+
+      <div className="spotlight">
+        {call && participantInSpotlight && (
+          <ParticipantView
+            participant={participantInSpotlight}
+            trackType={
+              hasScreenShare(participantInSpotlight)
+                ? 'screenShareTrack'
+                : 'videoTrack'
+            }
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+// utility to determine whether the participant in spotlight is sharing their screen
+const hasScreenShare = (p: StreamVideoParticipant) =>
+  p.publishedTracks.includes(SfuModels.TrackType.SCREEN_SHARE);
